513f486b50b6949375a85674c6a6386b010711a1
Jerome Coussanes <j.coussanes@gmail.com>
2014-05-21 15:40:16 +0200

    Rename variables and organize the source code

--- src/lo43_p14_coussanes_thibaud/UserInterface.java
+++ src/lo43_p14_coussanes_thibaud/UserInterface.java
@@ -26,82 +26,76 @@ import java.awt.Color;
 import net.miginfocom.swing.MigLayout;
 
 public class UserInterface extends JFrame{
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField strStart;
+	private JTextField strEnd;
+	private JPanel container;
+	
 	public UserInterface() {
 		
+		//Initialization relative to the window
+		container=new JPanel();
 		this.setTitle("Map manager");
 	    this.setSize(720, 520);
 	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    this.setLocationRelativeTo(null);
+	    this.setContentPane(container);
+	    container.setLayout(new BorderLayout(0, 0));
 	    
+	    //Creation of every part of the user interface
+	    	//Menu bar
 	    JMenuBar menuBar = new JMenuBar();
-	    getContentPane().add(menuBar, BorderLayout.NORTH);
+	    JMenu mnFile = new JMenu("Fichier");
+	    JMenuItem importXML = new JMenuItem("Importer XML");
+	    JMenuItem exportXML = new JMenuItem("Exporter XML");
+	    JMenuItem quit = new JMenuItem("Quiter");
+	    JMenu mnHelp = new JMenu("Aide");
+	    JMenuItem help = new JMenuItem("Aide à l'utilistation");
+	    	//Information panel
+	    JPanel infoPanel = new JPanel();
+	    JLabel lStart = new JLabel("Départ");
+	    JLabel lEnd = new JLabel("Arrivée");
+	    strStart = new JTextField();
+	    strEnd = new JTextField();
+	    JButton btnCalculate = new JButton("Calculer");
+	    JTextArea mapInformation = new JTextArea();
+	    JLabel lZoom = new JLabel("100%");
+	    JSlider slidZoom = new JSlider();
+	    JButton btnTrue = new JButton("Réelle");
+	    JButton btnGlobal = new JButton("Globale");
+	    	//Map view panel
+	    JPanel mapPanel = new JPanel();
 	    
-	    JMenu mnFichier = new JMenu("Fichier");
-	    menuBar.add(mnFichier);
 	    
-	    JMenuItem mntmNewMenuItem = new JMenuItem("patate");
-	    mnFichier.add(mntmNewMenuItem);
-	    
-	    JMenuItem mntmNewMenuItem_1 = new JMenuItem("tomate");
-	    mnFichier.add(mntmNewMenuItem_1);
-	    
-	    JMenuItem mntmNewMenuItem_2 = new JMenuItem("New menu item");
-	    mnFichier.add(mntmNewMenuItem_2);
-	    
-	    JMenu mnAide = new JMenu("Aide");
-	    menuBar.add(mnAide);
-	    
-	    JMenuItem mntmAide = new JMenuItem("aide ");
-	    mnAide.add(mntmAide);
-		
-	    JPanel panel = new JPanel();
-		getContentPane().add(panel, BorderLayout.WEST);
-		panel.setLayout(new MigLayout("", "[31.00px][78.00px]", "[27.00px][27.00px][29.00px][26.00px][27.00px][201.00px,grow][26.00px][20.00px][33.00px]"));
-		
-		JLabel lblNewLabel = new JLabel("Départ");
-		panel.add(lblNewLabel, "cell 0 0 2 1");
-		
-		textField = new JTextField();
-		panel.add(textField, "cell 0 1 2 1,growx,aligny center");
-		textField.setColumns(10);
-		
-		JLabel lblNewLabel_1 = new JLabel("Arrivée");
-		panel.add(lblNewLabel_1, "cell 0 2 2 1");
-		
-		textField_1 = new JTextField();
-		panel.add(textField_1, "cell 0 3 2 1,growx,aligny center");
-		textField_1.setColumns(10);
-		
-		JButton btnNewButton_1 = new JButton("Calculer");
-		panel.add(btnNewButton_1, "cell 0 4 2 1,alignx center,aligny center");
-		
-		JTextArea textArea = new JTextArea();
-		textArea.setEditable(false);
-		panel.add(textArea, "cell 0 5 2 1,grow");
-		
-		JLabel lblNewLabel_2 = new JLabel("100%");
-		panel.add(lblNewLabel_2, "cell 0 6 2 1");
-		
-		JSlider slider = new JSlider();
-		panel.add(slider, "cell 0 7 2 1");
-		
-		JButton btnNewButton = new JButton("Réelle");
-		panel.add(btnNewButton, "cell 0 8,alignx center,aligny center");
-		
-		JButton btnNewButton_2 = new JButton("Globale");
-		panel.add(btnNewButton_2, "cell 1 8,alignx center,aligny center");
-		
-		JPanel panel_1 = new JPanel();
-		getContentPane().add(panel_1, BorderLayout.CENTER);
-		panel_1.setLayout(new GridLayout(0, 1, 0, 0));
-		
-		JLabel label_8 = new JLabel("");
-		panel_1.add(label_8);
-		
-		JLabel label_5 = new JLabel("");
-		panel_1.add(label_5);
-		
+	    //Graphic interface element settings
+	    	//Menu bar
+	    container.add(menuBar, BorderLayout.NORTH);  
+	    menuBar.add(mnFile);	    
+	    mnFile.add(importXML);	    
+	    mnFile.add(exportXML);
+	    mnFile.add(quit);
+	    menuBar.add(mnHelp);
+	    mnHelp.add(help);
+	    	//Information panel
+		container.add(infoPanel, BorderLayout.WEST);
+		infoPanel.setLayout(new MigLayout("", "[31.00px][78.00px]", "[27.00px][27.00px][29.00px][26.00px][27.00px][201.00px,grow][26.00px][20.00px][33.00px]"));
+		infoPanel.add(lStart, "cell 0 0 2 1");
+		infoPanel.add(strStart, "cell 0 1 2 1,growx,aligny center");
+		strStart.setColumns(10);
+		infoPanel.add(lEnd, "cell 0 2 2 1");
+		infoPanel.add(strEnd, "cell 0 3 2 1,growx,aligny center");
+		strEnd.setColumns(10);
+		infoPanel.add(btnCalculate, "cell 0 4 2 1,alignx center,aligny center");
+		mapInformation.setEditable(false);
+		infoPanel.add(mapInformation, "cell 0 5 2 1,grow");
+		infoPanel.add(lZoom, "cell 0 6 2 1");	
+		infoPanel.add(slidZoom, "cell 0 7 2 1");
+		infoPanel.add(btnTrue, "cell 0 8,alignx center,aligny center");
+		infoPanel.add(btnGlobal, "cell 1 8,alignx center,aligny center");
+			//Map view panel
+		container.add(mapPanel);
+		mapPanel.setLayout(new GridLayout(0, 1, 0, 0));
+
+		//Set the full interface visible
 		this.setVisible(true);
 	}
 
